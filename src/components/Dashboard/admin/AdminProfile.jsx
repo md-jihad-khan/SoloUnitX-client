a257065b26a390569559538d3dfe6cdf6b369a1c
md-jihad-khan <mdjihadkhan9867@gmail.com>
2024-06-06 02:49:08 +0600

    created the admin profile page with all statistic

--- src/components/Dashboard/admin/AdminProfile.jsx
+++ src/components/Dashboard/admin/AdminProfile.jsx
@@ -1,9 +1,33 @@
 import { useContext } from "react";
 import { AuthContext } from "../../../providers/AuthProvider";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaUser, FaUsers } from "react-icons/fa";
+import {
+  FaHouse,
+  FaHouseCircleCheck,
+  FaHouseCircleXmark,
+} from "react-icons/fa6";
+import Heading from "../../shared/Heading";
 
 const AdminProfile = () => {
   const { user } = useContext(AuthContext);
   const [username, domain] = user.email.split("@");
+  const axiosSecure = useAxiosSecure();
+  const { data: statistics = {}, isLoading } = useQuery({
+    queryKey: ["statistics"],
+    queryFn: async () => {
+      const result = await axiosSecure("/statistics");
+      return result.data;
+    },
+  });
+
+  if (isLoading)
+    return (
+      <div className="min-h-[70vh] flex items-center justify-center">
+        <span className="loading loading-spinner loading-lg text-yellow-500 text-center"></span>
+      </div>
+    );
 
   return (
     <>
@@ -25,6 +49,47 @@ const AdminProfile = () => {
           </span>
         </h2>
       </header>
+      <Heading title={"All Stats"} />
+      <hr />
+      <div className=" mt-8 ">
+        <div className="flex items-center justify-center flex-wrap gap-5 -m-4 text-center">
+          <div className="p-4 w-40 border-2 text-white bg-yellow-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110 ">
+            <FaUsers className="text-5xl mx-auto" />
+            <h2 className="title-font font-bold text-3xl ">
+              {statistics.totalUsers}
+            </h2>
+            <p className="leading-relaxed">Total Users</p>
+          </div>
+          <div className="p-4 w-40 border-2 text-white bg-yellow-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110 ">
+            <FaHouse className="text-5xl mx-auto" />
+            <h2 className="title-font font-bold text-3xl ">
+              {statistics.totalRooms}
+            </h2>
+            <p className="leading-relaxed">Total Rooms</p>
+          </div>
+          <div className="p-4 w-40 border-2 text-white bg-yellow-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110 ">
+            <FaUser className="text-5xl mx-auto" />
+            <h2 className="title-font font-bold text-3xl ">
+              {statistics.members}
+            </h2>
+            <p className="leading-relaxed">Total Members</p>
+          </div>
+          <div className="p-4 w-40 border-2 text-white bg-yellow-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110 ">
+            <FaHouseCircleCheck className="text-5xl mx-auto" />
+            <h2 className="title-font font-bold text-3xl ">
+              {parseFloat(statistics.percentageAvailable.toFixed(1))}%
+            </h2>
+            <p className="leading-relaxed">available rooms </p>
+          </div>
+          <div className="p-4 w-44 border-2 text-white bg-yellow-500 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-110 ">
+            <FaHouseCircleXmark className="text-5xl mx-auto" />
+            <h2 className="title-font font-bold text-3xl ">
+              {parseFloat(statistics.percentageUnavailable.toFixed(1))}%
+            </h2>
+            <p className="">Unavailable rooms </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- src/pages/Dashboard/admin/AgreementRequest.jsx
+++ src/pages/Dashboard/admin/AgreementRequest.jsx
@@ -6,28 +6,44 @@ import AgreementRequestCard from "../../../components/Dashboard/admin/AgreementR
 const AgreementRequest = () => {
   const axiosSecure = useAxiosSecure();
 
-  const { data: agreements = [], refetch } = useQuery({
+  const {
+    data: agreements = [],
+    refetch,
+    isLoading,
+  } = useQuery({
     queryKey: ["agreements"],
     queryFn: async () => {
       const result = await axiosSecure("/agreements");
-
       return result.data;
     },
   });
+
+  if (isLoading)
+    return (
+      <div className="min-h-[70vh] flex items-center justify-center">
+        <span className="loading loading-spinner loading-lg text-yellow-500 text-center"></span>
+      </div>
+    );
 
   return (
     <>
       <Heading title={"Agreement Request"} />
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-        {agreements.map((agreement) => (
-          <AgreementRequestCard
-            key={agreement._id}
-            refetch={refetch}
-            agreement={agreement}
-          />
-        ))}
-      </div>
+      {agreements.length > 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+          {agreements.map((agreement) => (
+            <AgreementRequestCard
+              key={agreement._id}
+              refetch={refetch}
+              agreement={agreement}
+            />
+          ))}
+        </div>
+      ) : (
+        <p className="font-bold text-xl text-center mt-20">
+          No Agreement Data available now
+        </p>
+      )}
     </>
   );
 };
